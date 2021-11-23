996f2cd6d1461b2b23efe845a44be6a7f0447aef
Larkoss <klarko01@ucy.ac.cy>
2021-11-23 14:42:25 +0200

    BugFix Authentication T1-Users

--- index.php
+++ index.php
@@ -29,19 +29,17 @@
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
 		      		<h3 class="mb-4 text-center">Observers System <br>Sign In</h3>
-		      		<form action="auth" method="post" class="signin-form">
+		      		<form action="auth.php" method="post" class="signin-form">
 		      			<div class="form-group">
-		      				<input type="text" name="username" class="form-control" placeholder="Username" required>
+		      				<input name="username" type="text" class="form-control" placeholder="Username" required>
 		      			</div>
 	            		<div class="form-group">
-	              			<input id="password-field" name=password type="password" class="form-control" placeholder="Password" required>
+	              			<input name="password" id="password-field" type="password" class="form-control" placeholder="Password" required>
 	             			 <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	            		</div>
 	            		<div class="form-group">
-	            			<button type="submit" name="Authenticate" class="form-control btn btn-primary submit px-3">Sign In</button>
+	            			<button type="submit" name="connect" class="form-control btn btn-primary submit px-3">Sign In</button>
 	            		</div>
-	            		<div class="form-group d-md-flex">
-	           			</div>
 	     			</form>
 		      		</div>
 				</div>
