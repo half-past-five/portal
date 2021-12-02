10f2d0d54ca6558d0bed4363a874f04c65495013
Larkoss <klarko01@ucy.ac.cy>
2021-12-02 18:01:00 +0200

    Update 0.Drop&Create_Tables.sql

--- inserts/0.Drop&Create_Tables.sql
+++ inserts/0.Drop&Create_Tables.sql
@@ -17,44 +17,85 @@ GO
 
 ---------CREATE TABLES----------
 
+---------- DROP DB ----------
 
-CREATE TABLE dbo.[T1-Question Questionnaire Pairs] (
-	[Question ID] int not null,
-	[Questionnaire ID] int not null,
-	UNIQUE([Question ID], [Questionnaire ID])
-	)
+--tables
+DECLARE @Sql NVARCHAR(500) DECLARE @Cursor CURSOR
+SET @Cursor = CURSOR FAST_FORWARD FOR
+SELECT DISTINCT sql = 'ALTER TABLE [' + tc2.TABLE_SCHEMA + '].[' +  tc2.TABLE_NAME + '] DROP [' + rc1.CONSTRAINT_NAME + '];'
+FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc1
+LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc2 ON tc2.CONSTRAINT_NAME =rc1.CONSTRAINT_NAME
+OPEN @Cursor FETCH NEXT FROM @Cursor INTO @Sql
+WHILE (@@FETCH_STATUS = 0)
+BEGIN
+Exec sp_executesql @Sql
+FETCH NEXT FROM @Cursor INTO @Sql
+END
+CLOSE @Cursor DEALLOCATE @Cursor
+GO
+EXEC sp_MSforeachtable 'DROP TABLE ?'
+GO
+
+--triggers
+DECLARE @sql VARCHAR(MAX)='';
+SELECT @sql=@sql+'drop trigger ['+name +'];' FROM sys.objects 
+WHERE type = 'tr' AND  is_ms_shipped = 0
+exec(@sql);
+GO
+
+
+--spocs
+DECLARE @sql VARCHAR(MAX)='';
+SELECT @sql=@sql+'drop procedure ['+name +'];' FROM sys.objects 
+WHERE type = 'p' AND  is_ms_shipped = 0
+exec(@sql);
+GO
+
+DROP VIEW IF EXISTS dbo.[Questions per Questionnaire]
+
+IF OBJECT_ID (N'dbo.canUserSeeQuestion', N'FN') IS NOT NULL DROP FUNCTION canUserSeeQuestion;
+IF OBJECT_ID (N'dbo.canUserSeeQuestionnaire', N'FN') IS NOT NULL DROP FUNCTION canUserSeeQuestionnaire; 
+IF OBJECT_ID (N'dbo.generateURL', N'FN') IS NOT NULL DROP FUNCTION generateURL;
+IF OBJECT_ID (N'dbo.canUserSeeUser', N'FN') IS NOT NULL DROP FUNCTION canUserSeeUser; 
+
+
+
+----------CREATE TABLES----------
+
+CREATE TABLE [dbo].[T1-Company](
+	[Company ID] int IDENTITY(1,1) not null,
+	[Registration Number] int not null, --NOT SPECIFIED BY US
+	[Brand Name] varchar(50) not null,
+	[Induction Date] date not null,
+	CONSTRAINT [PK-Company] PRIMARY KEY NONCLUSTERED ([Company ID]),
+	UNIQUE([Registration Number])
+)
 
 
 CREATE TABLE dbo.[T1-User] (
 	[User ID] int IDENTITY(1,1) not null, --IDENTITY added 
+	[IDCard] int not null, --ADD DEFAULT
 	[Name] varchar(30) not null,
 	[Birth Date] date not null,
 	Sex char(1) not null,
-	Position varchar(30) not null,
+	Position varchar(30) DEFAULT 'employee' not null,
 	Username varchar(30) not null,
 	[Password] varchar(30) not null,
 	Privilages int not null,
 	[Company ID] int,
-	[Manager ID] int,
+	[Manager ID] int DEFAULT NULL,
 	UNIQUE(Username),
 	CONSTRAINT [PK-User] PRIMARY KEY NONCLUSTERED ([User ID]),
 	CHECK ([Privilages] in ('1', '2', '3'))
 )
 
 
-CREATE TABLE [dbo].[T1-Company](
-	[Registration Number] int not null, --NOT SPECIFIED BY US
-	[Brand Name] varchar(50) not null,
-	[Induction Date] date not null,
-	CONSTRAINT [PK-Company] PRIMARY KEY NONCLUSTERED ([Registration Number])
-	)
-
-
 CREATE TABLE dbo.[T1-Question] (
 	[Question ID] int IDENTITY(1,1) not null,
+	[Question Code] varchar(30) not null,	
 	[Creator ID] int,
 	[Type] varchar(30),
-	[Description] varchar(50) not null,
+	[Description] varchar(50) DEFAULT 'This is decription' not null,
 	[Text] varchar(100) not null,
 	CONSTRAINT [PK-Question] PRIMARY KEY NONCLUSTERED ([Question ID]),
 	CHECK ([Type] in ('Free Text','Multiple Choice','Arithmetic'))
@@ -63,10 +104,18 @@ CREATE TABLE dbo.[T1-Question] (
 
 CREATE TABLE dbo.[T1-Free Text Question] (
 	[Question ID] int not null,
-	[Restriction] varchar(30)
+	[Question Code] varchar(30) not null,
+	[Restriction] varchar(30) DEFAULT null
 	UNIQUE([Question ID])
 )	
 
+CREATE TABLE dbo.[T1-Arithmetic Question] (
+	[Question ID] int not null,
+	[Question Code] varchar(30) not null,
+	[MIN value] int DEFAULT null, 
+	[MAX value] int DEFAULT null, --min & max value added for range	
+	CHECK ([MAX value] > [MIN value])
+)
 
 CREATE TABLE dbo.[T1-Multiple Choice Question] (
 	[Question ID] int not null,
@@ -79,16 +128,6 @@ CREATE TABLE dbo.[T1-Multiple Choice Answers] (
 	[Answer] varchar(50)
 	UNIQUE([Question ID], [Answer])
 	)
-	
-
-CREATE TABLE dbo.[T1-Arithmetic Question] (
-	[Question ID] int not null,
-	[MIN value] int not null, 
-	[MAX value] int not null, --min & max value added for range	
-	CHECK ([MAX value] > [MIN value])
-)
-
-
 
 CREATE TABLE [dbo].[T1-Questionnaire](
 	[Questionnaire ID] int IDENTITY(1,1) not null,
@@ -100,14 +139,19 @@ CREATE TABLE [dbo].[T1-Questionnaire](
 	UNIQUE (Title, [Version]),
 	CONSTRAINT [PK-Questionnaire] PRIMARY KEY NONCLUSTERED ([Questionnaire ID])
 	)
+
+	CREATE TABLE dbo.[T1-Question Questionnaire Pairs] (
+	[Question ID] int not null,
+	[Questionnaire ID] int not null,
+	UNIQUE([Question ID], [Questionnaire ID])
+	)
 	
 
 CREATE TABLE [dbo].[T1-Log](
 	[Event]	varchar(100) not null,
 	)
-	
-	
-	---------- INSERTS ----------
+
+---------- INSERTS ----------
 --RUN 1.[T1-Company]
 --RUN 2.[T1-User]
 --RUN 3.[T1-Question]
@@ -115,11 +159,10 @@ CREATE TABLE [dbo].[T1-Log](
 --RUN 7.[T1-Question Questionnaire Pairs]
 ---------- INSERTS ----------
 
-	
-	--FOREIGN KEYS 
+--FOREIGN KEYS 
 ALTER TABLE dbo.[T1-User] WITH NOCHECK ADD
 CONSTRAINT [FK-User-Manager] FOREIGN KEY ([Manager ID]) REFERENCES [T1-User]([User ID]), --TRIGGER
-CONSTRAINT [FK-User-Company] FOREIGN KEY ([Company ID]) REFERENCES [dbo].[T1-Company]([Registration Number]) ON UPDATE CASCADE ON DELETE CASCADE
+CONSTRAINT [FK-User-Company] FOREIGN KEY ([Company ID]) REFERENCES [dbo].[T1-Company]([Company ID]) ON UPDATE CASCADE ON DELETE CASCADE
 
 ALTER TABLE dbo.[T1-Question] ADD
 CONSTRAINT [FK-Question-CreatorUser] FOREIGN KEY ([Creator ID]) REFERENCES [dbo].[T1-User]([User ID]) ON UPDATE CASCADE ON DELETE SET NULL
