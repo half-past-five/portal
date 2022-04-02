feaa1555cdc6f7d70e265b8199c7502777d6119b
Larkoss <klarko01@ucy.ac.cy>
2022-04-02 22:48:53 +0200

    Bugfix authenticate doctors

--- logout.php
+++ logout.php
@@ -21,8 +21,8 @@ $connectionOptions = $_SESSION["connectionOptions"];
             background: #9C1092
         }
     </style>
-    <title>BYE - BYE!</title>
-    <link rel="icon" href="https://i.imgur.com/70ln1bX.png">
+    <title>Logging out!</title>
+    <link rel="icon" href="images/logo.png">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
@@ -30,7 +30,7 @@ $connectionOptions = $_SESSION["connectionOptions"];
     <link rel="stylesheet" href="css/style.css">
 </head>
 
-<body class="img js-fullheight" style="background-image: url(https://images.saymedia-content.com/.image/t_share/MTc4NzM1OTc4MzE0MzQzOTM1/how-to-create-cool-website-backgrounds-the-ultimate-guide.png);">
+<body class="img js-fullheight" style="background-image: url(images/background.jpg);">
     <section class="ftco-section">
         <div class="container">
             <div class="row justify-content-center">
@@ -38,8 +38,8 @@ $connectionOptions = $_SESSION["connectionOptions"];
                     <div class="login-wrap p-0">
                         <?php
                         if (isset($_POST['disconnect'])) {
-                            echo "<h3>Clossing session and redirecting to start page</h3></br></br>";
-                            echo "<h2 style='color:white'>Thank you for choosing EPL342 Team 1!<h2>";
+                            echo "<h3>Clossing session and redirecting to home page</h3></br></br>";
+                            echo "<h2 style='color:white'>Thank you for using HPF Portal!<h2>";
                             session_unset();
                             session_destroy();
                             die('<meta http-equiv="refresh" content="3.5; url=index.php" />');
